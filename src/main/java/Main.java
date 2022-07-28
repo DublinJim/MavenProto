529fb7d2c74e9b2c1d8779f0502ff1771cf38ed2
James Keogh <jkeogh@adga.ca>
2022-07-28 15:29:44 -0400

    linked objects to spacestation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,27 @@
 import proto.Captain;
 import proto.Captain.RaceType;
-import proto.SpaceStationOuter;
+import proto.SpaceShip;
+import proto.SpaceStation;
 
 public class Main {
 
     public static void main(String[] args) {
-        Captain.Builder builder = Captain.newBuilder().setName("Joe").setRace(RaceType.VULCAN);
+        Captain.Builder builder = Captain.newBuilder();
 
+        Captain joe = builder
+            .setName("Joe")
+            .setRace(RaceType.VULCAN)
+            .build();
 
-        System.out.println(builder.getName());
+        System.out.println(joe.getName());
+        SpaceStation.Builder ds9 = SpaceStation.newBuilder();
+
+        System.out.println(ds9.getCaptain());
+        SpaceShip.Builder boeing = SpaceShip.newBuilder();
+        boeing.setCrew(3).setCargo("gold").setName("Orion").build();
+
+        ds9.setCaptain(joe).addSpaceships(boeing).build();
+
+        System.out.println( ds9);
     }
 }
