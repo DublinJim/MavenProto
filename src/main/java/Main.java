e060f8cd3cf41ba7254af50d9ea07a0715840912
James Keogh <jkeogh@adga.ca>
2022-07-28 14:54:24 -0400

    first build of a Captain object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
+import proto.Captain;
+import proto.Captain.RaceType;
 import proto.SpaceStationOuter;
 
 public class Main {
 
     public static void main(String[] args) {
+        Captain.Builder builder = Captain.newBuilder().setName("Joe").setRace(RaceType.VULCAN);
 
 
-
-
-
+        System.out.println(builder.getName());
     }
 }
